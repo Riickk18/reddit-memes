b001bdbed834b4c47cf345ead167236dd77a37cc
Richard Pacheco <richard.pacheco@citi.com.mx>
2023-05-09 21:04:39 -0400

    Pod configurations and swiftlint

---
+++ riickk18-reddit-memes/Pods/Target Support Files/Pods-riickk18-reddit-memes/Pods-riickk18-reddit-memes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_riickk18_reddit_memes : NSObject
+@end
+@implementation PodsDummy_Pods_riickk18_reddit_memes
+@end
---
+++ riickk18-reddit-memes/Pods/Target Support Files/Pods-riickk18-reddit-memesTests/Pods-riickk18-reddit-memesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_riickk18_reddit_memesTests : NSObject
+@end
+@implementation PodsDummy_Pods_riickk18_reddit_memesTests
+@end
