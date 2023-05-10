518523291b85502309f1f68cb878472793243be2
Richard <richardorlan95@gmail.com>
2023-05-10 16:20:22 -0400

    feat: added first configuration for meme view model

--- riickk18-reddit-memes/riickk18-reddit-memes/ViewModel/Home/MemeCell/MemeCellViewModel.swift
+++ riickk18-reddit-memes/riickk18-reddit-memes/ViewModel/Home/MemeCell/MemeCellViewModel.swift
@@ -9,5 +9,12 @@ import Foundation
 import SwiftUI
 
 class MemeCellViewModel: ObservableObject {
-    
+    @Published var image: String = ""
+    @Published var title: String = ""
+    @Published var description: String = ""
+    @Published var likeCounter: Int = 0
+    @Published var commentsCounter: Int = 0
+    @Published var featurePeople: [String] = []
+    @Published var ownerImage: String = ""
+    @Published var ownerName: String = ""
 }
