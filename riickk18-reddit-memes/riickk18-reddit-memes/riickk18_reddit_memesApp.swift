280bd1d969a8063fff794e6244ccbce0784e89d8
Richard Pacheco <richard.pacheco@citi.com.mx>
2023-05-09 20:15:19 -0400

    First commit with project definition

---
+++ riickk18-reddit-memes/riickk18-reddit-memes/riickk18_reddit_memesApp.swift
@@ -0,0 +1,17 @@
+//
+//  riickk18_reddit_memesApp.swift
+//  riickk18-reddit-memes
+//
+//  Created by Richard Pacheco on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct riickk18_reddit_memesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
